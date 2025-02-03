390718fba6c92090a08f7f9f75c2607b85087670
Misaki-K-run <rikkahuru3@gmail.com>
2025-02-03 13:58:13 +0900

    post_cardの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :memo, length: { maximum: 1000 }
   validates :sum, length: { maximum: 100000 }
 
+  # 関連付け
   belongs_to :user
 end
--- db/migrate/20250202042353_create_posts.rb
+++ db/migrate/20250202042353_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.2]
   def change
     create_table :posts do |t|
-      t.text :memo
-      t.integer :sum
+      t.text :memo, null: true
+      t.integer :sum, null: true
       t.references :user, foreign_key: true
 
       t.timestamps
