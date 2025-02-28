794551efffe1ad937178374e13d201724e95eb56
Misaki-K-run <rikkahuru3@gmail.com>
2025-02-28 13:02:13 +0900

    add:お気に入り機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,11 +59,14 @@ class PostsController < ApplicationController
     end
   end
 
-
   def destroy
     post = current_user.posts.find(params[:id])
     post.destroy!
     redirect_to posts_path, notice: "投稿を削除しました", status: :see_other
+  end
+
+  def bookmarks
+    @bookmark_posts = current_user.bookmark_posts.includes(:user).order(created_at: :desc)
   end
 
   private
