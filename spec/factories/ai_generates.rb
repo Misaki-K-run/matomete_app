55823f984dcf6ca29aa0b45f7aa2fefad28ecf33
Misaki-K-run <rikkahuru3@gmail.com>
2025-04-05 17:55:59 +0900

    add:ユーザー、投稿作成リクエストテスト

--- spec/factories/ai_generates.rb
+++ spec/factories/ai_generates.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :ai_generate do
     budget_request { 10000 }
     people_request { 4 }
-    menu_response { { monday: 'Pasta', tuesday: 'Pizza' }.to_json } # 必要に応じて値を変更
-    shopping_list_response { { meat: 'Chicken', vegetable: 'Carrot' }.to_json } # 必要に応じて値を変更
+    menu_response { { monday: 'Pasta', tuesday: 'Pizza' }.to_json }
+    shopping_list_response { { meat: 'Chicken', vegetable: 'Carrot' }.to_json }
     sum_response { 20000 }
     association :user
   end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     memo { Faker::String.random(length: 20) }
     sum { Faker::Number.binary(digits: 5) }
     association :user
+
+    trait :with_menu do
+      after(:create) do |post|
+        create(:menu, post: post)
+      end
+    end
   end
 end
